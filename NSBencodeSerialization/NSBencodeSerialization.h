f239515f51e9d8e570ec13cb550ef58d0f365813
Qata Life <qatagee@gmail.com>
2014-02-27 19:54:43 +1100

    First commit.

--- NSBencodeSerialization/NSBencodeSerialization.h
+++ NSBencodeSerialization/NSBencodeSerialization.h
@@ -1,13 +1,16 @@
 //
 //  NSBencodeSerialization.h
-//  NSBencodeSerialization
+//  DALILite
 //
-//  Created by Carlo Tortorella on 27/02/2014.
-//  Copyright (c) 2014 Carlo Tortorella. All rights reserved.
+//  Created by Carlo Tortorella on 16/12/13.
+//  Copyright (c) 2013 Carlo Tortorella. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 @interface NSBencodeSerialization : NSObject
+
++ (id)bencodedObjectWithData:(NSData *)data error:(NSError *__autoreleasing *)error;
++ (NSData *)dataWithBencodedObject:(id)object error:(NSError *__autoreleasing *)error;
 
 @end
